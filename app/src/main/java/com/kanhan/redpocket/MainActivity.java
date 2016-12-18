3aa853ef731008d280e0eef24c0d6ac931af3bc4
jting217 <jting217@gmail.com>
2016-12-18 20:24:11 +0800

    Change BottomNavigationView switch frangment method

--- app/src/main/java/com/kanhan/redpocket/MainActivity.java
+++ app/src/main/java/com/kanhan/redpocket/MainActivity.java
@@ -18,10 +18,13 @@ import android.widget.TextView;
 import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String SELECTED_ITEM = "arg_selected_item";
 
     private TextView textFavorites;
     private TextView textSchedules;
     private TextView textMusic;
+    private BottomNavigationView mBottomNav;
+    private int mSelectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,61 +33,121 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mBottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.disableShiftMode(mBottomNav);
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return true;
+            }
+        });
+        MenuItem selectedItem;
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            selectedItem = mBottomNav.getMenu().findItem(mSelectedItem);
+        } else {
+            selectedItem = mBottomNav.getMenu().getItem(0);
+        }
+        selectFragment(selectedItem);
+
 //        textFavorites = (TextView) findViewById(R.id.text_favorites);
 //        textSchedules = (TextView) findViewById(R.id.text_schedules);
 //        textMusic = (TextView) findViewById(R.id.text_music);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)
-                findViewById(R.id.bottom_navigation);
-        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
-        Log.d("☆","start");
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    Fragment frag = null;
-                    FragmentTransaction ft;
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        //item.setIcon(R.drawable.ic_access_time_white_24dp);
-                        //item.set
+//        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+//                findViewById(R.id.bottom_navigation);
+//        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+//        Log.d("☆","start");
+//        bottomNavigationView.setOnNavigationItemSelectedListener(
+//                new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                    Fragment frag = null;
+//                    FragmentTransaction ft;
+//                    @Override
+//                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                        //item.setIcon(R.drawable.ic_access_time_white_24dp);
+//                        //item.set
+//
+//                        switch (item.getItemId()) {
+//                            case R.id.paly:
+//                                frag = MenuFragment.newInstance(getString(R.string.text_play),
+//                                        getColorFromRes(R.color.color_home));
+//                                ft = getSupportFragmentManager().beginTransaction();
+//                                ft.add(R.id.container, frag, frag.getTag());
+//                                ft.commit();
+//
+//                                Log.d("☆","1");
+//
+//                                break;
+//                            case R.id.shops:
+//                                frag = MenuFragment.newInstance(getString(R.string.text_shops),
+//                                        getColorFromRes(R.color.color_notifications));
+//                                Log.d("☆","2");
+//                                ft = getSupportFragmentManager().beginTransaction();
+//                                ft.add(R.id.container, frag, frag.getTag());
+//                                ft.commit();
+//
+//                                break;
+//                            case R.id.league:
+//                                frag = MenuFragment.newInstance(getString(R.string.text_league),
+//                                        getColorFromRes(R.color.color_home));
+//                                Log.d("☆","3");
+//                                ft = getSupportFragmentManager().beginTransaction();
+//                                ft.add(R.id.container, frag, frag.getTag());
+//                                ft.commit();
+////                                textFavorites.setVisibility(View.GONE);
+////                                textSchedules.setVisibility(View.GONE);
+////                                textMusic.setVisibility(View.VISIBLE);
+//                                break;
+//                        }
+//                        return false;
+//                    }
+//
+//
+//                });
 
-                        switch (item.getItemId()) {
-                            case R.id.paly:
-                                frag = MenuFragment.newInstance(getString(R.string.text_play),
-                                        getColorFromRes(R.color.color_home));
-                                ft = getSupportFragmentManager().beginTransaction();
-                                ft.add(R.id.container, frag, frag.getTag());
-                                ft.commit();
+    }
 
-                                Log.d("☆","1");
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        // init corresponding fragment
+        switch (item.getItemId()) {
+            case R.id.paly:
+                frag = MenuFragment.newInstance(getString(R.string.text_play),
+                        getColorFromRes(R.color.color_home));
+                break;
+            case R.id.shops:
+                frag = MenuFragment.newInstance(getString(R.string.text_shops),
+                        getColorFromRes(R.color.color_notifications));
+                break;
+            case R.id.league:
+                frag = MenuFragment.newInstance(getString(R.string.text_league),
+                        getColorFromRes(R.color.color_search));
+            case R.id.options:
+                frag = MenuFragment.newInstance(getString(R.string.text_options),
+                        getColorFromRes(R.color.color_search));
+            case R.id.info:
+                frag = MenuFragment.newInstance(getString(R.string.text_info),
+                        getColorFromRes(R.color.color_search));
+                break;
+        }
 
-                                break;
-                            case R.id.shops:
-                                frag = MenuFragment.newInstance(getString(R.string.text_shops),
-                                        getColorFromRes(R.color.color_notifications));
-                                Log.d("☆","2");
-                                ft = getSupportFragmentManager().beginTransaction();
-                                ft.add(R.id.container, frag, frag.getTag());
-                                ft.commit();
+        // update selected item
+        mSelectedItem = item.getItemId();
 
-                                break;
-                            case R.id.league:
-                                frag = MenuFragment.newInstance(getString(R.string.text_league),
-                                        getColorFromRes(R.color.color_home));
-                                Log.d("☆","3");
-                                ft = getSupportFragmentManager().beginTransaction();
-                                ft.add(R.id.container, frag, frag.getTag());
-                                ft.commit();
-//                                textFavorites.setVisibility(View.GONE);
-//                                textSchedules.setVisibility(View.GONE);
-//                                textMusic.setVisibility(View.VISIBLE);
-                                break;
-                        }
-                        return false;
-                    }
+        // uncheck the other items.
+        for (int i = 0; i< mBottomNav.getMenu().size(); i++) {
+            MenuItem menuItem = mBottomNav.getMenu().getItem(i);
+            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
+        }
 
+        updateToolbarText(item.getTitle());
 
-                });
-
+        if (frag != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.container, frag, frag.getTag());
+            ft.commit();
+        }
     }
 
     private void updateToolbarText(CharSequence text) {
